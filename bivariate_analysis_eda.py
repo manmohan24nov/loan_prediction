55898023a5d80ed5047cf1bd2af8eab0629fbb85
Manmohan Singh <manmohan2493singh@yahoo.in>
2019-02-11 00:25:36 +0530

    Create bivariate_analysis_eda.py

---
+++ bivariate_analysis_eda.py
@@ -0,0 +1,102 @@
+import loan_data
+train = loan_data.main()
+print(train.columns)
+
+###subplotting refrence website
+#http://jonathansoma.com/lede/data-studio/classes/small-multiples/long-explanation-of-using-plt-subplots-to-create-small-multiples/
+
+##Lets recall some of the hypotheses that we generated earlier:
+#Applicants with high income should have more chances of loan approval.
+#Applicants who have repaid their previous debts should have higher chances of loan approval.
+#Loan approval should also depend on the loan amount. If the loan amount is less, chances of loan approval should be high.
+#Lesser the amount to be paid monthly to repay the loan, higher the chances of loan approval.
+
+#Lets try to test the above mentioned hypotheses using bivariate analysis
+#After looking at every variable individually in univariate analysis, we will now explore them again with respect to the target variable.
+#Categorical Independent Variable vs Target Variable
+
+#First of all we will find the relation between target variable and categorical independent variables.
+# Let us look at the stacked bar plot now which will give us the proportion of approved and unapproved loans.
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# plt.figure(1)
+Gender=pd.crosstab(train['Gender'],train['Loan_Status'])
+Gender.div(Gender.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+plt.show()
+
+Married=pd.crosstab(train['Married'],train['Loan_Status'])
+Dependents=pd.crosstab(train['Dependents'],train['Loan_Status'])
+Education=pd.crosstab(train['Education'],train['Loan_Status'])
+Self_Employed=pd.crosstab(train['Self_Employed'],train['Loan_Status'])
+
+Married.div(Married.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+plt.show()
+
+Dependents.div(Dependents.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.show()
+
+Education.div(Education.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+plt.show()
+
+Self_Employed.div(Self_Employed.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+
+Credit_History=pd.crosstab(train['Credit_History'],train['Loan_Status'])
+Property_Area=pd.crosstab(train['Property_Area'],train['Loan_Status'])
+
+Credit_History.div(Credit_History.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True, figsize=(4,4))
+plt.show()
+
+Property_Area.div(Property_Area.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.show()
+
+####Numerical Independent Variable vs Target Variable
+
+train.groupby('Loan_Status')['ApplicantIncome'].mean().plot.bar()
+
+fig, axes = plt.subplots(nrows=2, ncols=2)
+
+bins=[0,2500,4000,6000,81000]
+group=['Low','Average','High', 'Very high']
+train['Income_bin']=pd.cut(train['ApplicantIncome'],bins,labels=group)
+Income_bin=pd.crosstab(train['Income_bin'],train['Loan_Status'])
+Income_bin.div(Income_bin.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True,ax=axes[0,0])
+plt.xlabel('ApplicantIncome')
+P = plt.ylabel('Percentage')
+
+bins=[0,1000,3000,42000]
+group=['Low','Average','High']
+train['Coapplicant_Income_bin']=pd.cut(train['CoapplicantIncome'],bins,labels=group)
+Coapplicant_Income_bin=pd.crosstab(train['Coapplicant_Income_bin'],train['Loan_Status'])
+Coapplicant_Income_bin.div(Coapplicant_Income_bin.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.xlabel('CoapplicantIncome')
+P = plt.ylabel('Percentage')
+
+train['Total_Income']=train['ApplicantIncome']+train['CoapplicantIncome']
+bins=[0,2500,4000,6000,81000]
+group=['Low','Average','High', 'Very high']
+train['Total_Income_bin']=pd.cut(train['Total_Income'],bins,labels=group)
+Total_Income_bin=pd.crosstab(train['Total_Income_bin'],train['Loan_Status'])
+Total_Income_bin.div(Total_Income_bin.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.xlabel('Total_Income')
+P = plt.ylabel('Percentage')
+
+bins=[0,100,200,700]
+group=['Low','Average','High']
+train['LoanAmount_bin']=pd.cut(train['LoanAmount'],bins,labels=group)
+LoanAmount_bin=pd.crosstab(train['LoanAmount_bin'],train['Loan_Status'])
+LoanAmount_bin.div(LoanAmount_bin.sum(1).astype(float), axis=0).plot(kind="bar", stacked=True)
+plt.xlabel('LoanAmount')
+P = plt.ylabel('Percentage')
+
+train=train.drop(['Income_bin', 'Coapplicant_Income_bin', 'LoanAmount_bin', 'Total_Income_bin', 'Total_Income'], axis=1)
+train['Dependents'].replace('3+', 3,inplace=True)
+train['Loan_Status'].replace('N', 0,inplace=True)
+train['Loan_Status'].replace('Y', 1,inplace=True)
+matrix = train.corr()
+f, ax = plt.subplots(figsize=(9, 6))
+sns.heatmap(matrix, vmax=.8, square=True, cmap="BuPu");
+
+plt.show()
